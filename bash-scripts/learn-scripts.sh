6344051fc13f0e8697b185b4b200d46a4aacaddd
Kumar Nitil <kumarnitildevops@gmail.com>
2022-04-24 09:12:12 +0000

    Bash-Scripting first commit

---
+++ bash-scripts/learn-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "TechMaestro Welcomes you to learn Bash Scripting\n"
+echo -e "Courtesy shredded_coder\n"
+
+echo "The uptime of the system is"
+uptime
+
+echo -e "\nThe memory utilization is"
+free -m
+
+echo -e "\nThe disk utilization is"
+df -h
