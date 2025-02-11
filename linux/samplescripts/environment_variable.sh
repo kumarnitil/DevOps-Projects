995ba40894d8174077aa417afb03b6b25f0365d0
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-02-11 03:04:43 +0000

    11th feb morning

--- linux/samplescripts/environment_variable.sh
+++ linux/samplescripts/environment_variable.sh
@@ -3,3 +3,5 @@
 echo "The environment variable PATH is $PATH"
 echo "The environment variable HOME is $HOME"
 echo "The environment variable USER is $USER"
+echo "The environment variable SHELL tells us which SHELL we are using $SHELL"
+cat /etc/shells # tells us all the available shells"
