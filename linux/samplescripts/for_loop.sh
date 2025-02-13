3a888b932a8607563c73f7fe5395d613cac722b7
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-02-13 15:11:02 +0000

    13th_Feb1

--- linux/samplescripts/for_loop.sh
+++ linux/samplescripts/for_loop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-for state in Ap Kar Mah WB JH UP DEL
+list="'Andhra' Kar MH Goa pan del"
+list="$list GJ"
+for state in $list
 do 
 	echo "The state name is $state"
 	sleep 4
 done
+
+#state=GJ
+echo "The state name finally is $state" # The variable reassignment is happening here:wq!
+
