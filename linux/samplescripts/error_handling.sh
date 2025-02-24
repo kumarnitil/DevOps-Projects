008507b54c55626ec6cc52464cdc936fedb1bac7
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-02-24 18:19:18 +0000

    Mon Feb 24 06:19:18 PM UTC 2025

---
+++ linux/samplescripts/error_handling.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# if the exit status is equal to zero then the command executed successfully
+# if the exit status is not zero then the command did not execute successfully
+
+# The following function below is an example of error handling
+
+handle_error() {
+	local error_message="$1"
+	echo "Error: $error_message"
+	# You can add additional logging or corrective actions here
+}
+
+{
+	echo "Attempting to run command1"
+	# date # Using the actual command
+	date? # Use this command when you want to capture actual command
+} || {
+	# Handles the error when command fails to execute
+        handle_error "Commands fails to execute"
+}
+
+echo "The script execution has completed"
+
