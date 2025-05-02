57b4d8b159dcb4c23c0ddc3f7f99812198f1c55c
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-05-02 15:49:25 +0530

    03-05-2025v2

--- linux/samplescripts/writing_data.sh
+++ linux/samplescripts/writing_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Hello, World" > output.txt
-echo "Append the Data" >> output.txt
+#echo "Hello, World" > output.txt # Deletes all the old contents and rewrites in the file
+echo "Append the Data" >> output.txt # Adds to the content of the file without erasing the data
 cat output.txt
