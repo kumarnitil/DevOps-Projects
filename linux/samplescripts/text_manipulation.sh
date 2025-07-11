3c02cc339f5ac7335ca072c94d4325dbc9cdf8c4
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-07-11 03:45:48 +0000

    11th July 2025

--- linux/samplescripts/text_manipulation.sh
+++ linux/samplescripts/text_manipulation.sh
@@ -4,4 +4,11 @@ echo "Hello Mr. $name"
 echo "Hello Mr. ${name:0:5}" # Will print only first five characters
 echo "Hello Mr. ${name/Nitil/Kumar}" # Will replace with Kumar
 echo "Hello Mr. ${name,,}" #Converts to lower case
-echo "Hello Mr. ${name^^}" #Convers to Upper Case
+echo "Hello Mr. ${name^^}" #Converts to Upper Case
+
+read -p "Enter your wife's name " name
+echo "Hello Mrs. $name"
+echo "Hello Mrs. ${name:0:10}"
+echo "Hello Mrs. ${name/Rajnandini/Gudiya}"
+echo "Hello Mrs. ${name,,}"
+echo "Hello Mrs. ${name^^}"
--- linux/samplescripts/writing_data.sh
+++ linux/samplescripts/writing_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Hello, World" > output.txt
 echo "Append the Data" >> output.txt
+echo "Starting with the Captstone Project" >> output.txt
 cat output.txt
