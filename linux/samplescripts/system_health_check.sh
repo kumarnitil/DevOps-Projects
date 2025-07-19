83a0713bf26c70756fe640f524ce1d4a8dbe2a6d
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-07-19 13:48:25 +0000

    18th July

--- linux/samplescripts/system_health_check.sh
+++ linux/samplescripts/system_health_check.sh
@@ -16,4 +16,5 @@ netstat -tuln
 # -t displays the TCP Connections
 # -u Shows only UDP Connections
 # -l Lists only listening port
-# -n displays numerical addressess instead of trying to resolve hostnames.
+# -n displays numerical addressess instead of trying to resolve hostnamesu.
+# https://www.google.com/search?udm=50&aep=11&q=top+-bn&mtid=SJZ7aIOELcWTseMP76_WmQE&mstk=AUtExfBAaYVohSCiaZemoT8d79sE-lTs3zSlYaebk11n4WYp869W3VAVXB84JDTymiaTXFr97rebGehY0OpQVG0tfmmYsNtgLsdz99WdZCiFLfwnSo9hhYuSHxwSWzLKGEccbfxqKDno6uoqhaU2Gmyi-kkAc2Xq_1QSPAT3OfTe3Kx0fgPMiVdxDbdzB4HboCv1SEUFtiw9kavLw4KbuWGCw0QMm3XOqJHxZvY4N0sPLDSSo0ML3bMTTclIK9WUzQGl44cyhiZX7rwRQDGEvpnCf--9clXQgBGi0aV8n5wekF3RDhYVlISu7OGXXBQNHcLD2hpa4aOCRByIBQ&csuir=1
--- linux/samplescripts/writing_data.sh
+++ linux/samplescripts/writing_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #echo "Hello, World" > output.txt # Deletes all the old contents and rewrites in the file
-echo "Append the Data" >> output.txt # Adds to the content of the file without erasing the data into a new line in the file.
+# echo "Append the Data" >> output.txt # Adds to the content of the file without erasing the data into a new line in the file.
+# cat output.txt
+echo "Complete all the project of Linux by 30th July" >> output.txt
 cat output.txt
