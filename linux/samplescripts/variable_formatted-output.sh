340ccece38159c22c92ff58ffd4ede4cde2d9c89
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-07-19 14:28:55 +0000

    19th July

--- linux/samplescripts/variable_formatted-output.sh
+++ linux/samplescripts/variable_formatted-output.sh
@@ -8,3 +8,12 @@ printf "%s\n" $fruits
 readonly NAME="Nitil" # will make the variable NAME as immutable
 declare -r NAME1="Nitil" # We can declare immutable variable with the keyword r as well
 
+# Second saet of variable declaration.
+: 'Variables in Bash are primarily treated as strings by default
+Thus they need to be converted into various other data types or
+declared as various other data types
+- String can be declared with or without quotes but using the quotes is recommended for strings containing spaces.
+'
+# -i is used to declare a variable as an integer
+declare -i num2=60
+printf "%d\n" $num2
