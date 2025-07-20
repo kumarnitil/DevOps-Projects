2230ad1fbfcef9cdbfb079748df2b6dcfed1f857
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-07-20 16:47:00 +0000

    Sun Jul 20 04:47:00 PM UTC 2025

--- linux/samplescripts/command_line_substitution.sh
+++ linux/samplescripts/command_line_substitution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Command line susitution"
+echo "Command line subsitution"
 echo "Today date is $(date)"
 echo "Use command line substitution to check the status of nginx service"
 status=" The nginx service is $(systemctl is-active nginx)"
