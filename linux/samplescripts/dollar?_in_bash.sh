bb610719a584b286fbab022ebebe364280844149
kumarnitil@gmail.com <kumarnitil@gmail.com>
2025-07-21 16:45:22 +0000

    Mon Jul 21 04:45:22 PM UTC 2025

--- linux/samplescripts/dollar?_in_bash.sh
+++ linux/samplescripts/dollar?_in_bash.sh
@@ -2,6 +2,7 @@
 # In Bash scripting, the special variable $? (dollar question mark) holds the exit status (or return value) of the last executed command
 # 0 (Zero): Indicates the command executed successfully without any errors.
 # Non-zero (1-255): Indicates that the command failed or encountered an error.
+# $? has the limitation that it cannot return a value greater than 255 and hence we need to use echo statment more correctly to return values in a function.
 ls /home
 if [ $? -eq 0 ]; then
 	echo "Command succeeded"
